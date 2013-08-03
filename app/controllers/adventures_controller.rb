339ca7b276f30635d23a6088c298e11b78c12970
JunaidQ <xcoder27@gmail.com>
2013-08-04 02:38:01 +0500

    home-action

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,16 +1,19 @@
 class AdventuresController < ApplicationController
 
-  def new
-  	@adventure = Adventure.new
-  end
+	def home
+	end	
 
-  def create
-  	@adventure = Adventure.new(params[:adventure])
-      if @adventure.save
-      	redirect_to new_adventure_path, notice: 'Adventure was successfully created.'
-      else
-      	render action: "new"
-      end
-  end
+	def new
+		@adventure = Adventure.new
+	end
+
+	def create
+		@adventure = Adventure.new(params[:adventure])
+	  if @adventure.save
+	  	redirect_to new_adventure_path, notice: 'Adventure was successfully created.'
+	  else
+	  	render action: "new"
+	  end
+	end
 
 end
